d3e326e599c695ab4723397a342b0ce9e4aa83a7
Michael Heise <mh.mh@web.de>
2023-07-01 17:51:29 +0200

    replace MD5 use by SHA256 in pflh.py

--- pflh.py
+++ pflh.py
@@ -7,7 +7,7 @@ __version__ = "0.0.1"
 __date__ = "07/01/2022"
 
 """List files matching a pattern in a directory and its sub-directories,
-and print results including file information (with MD5 hash)
+and print results including file information (with SHA256 hash)
 to stdout or save as a CSV file.
 """
 
@@ -22,37 +22,37 @@ import pfllib.pflparams as pflparams
 import pfllib.pflrun as pflrun
 
 
-class PFLRunFileInfoWithMD5(pflrun.PFLRun):
-    """Derived class for scanning and storage of file information including MD5 hash."""
+class PFLRunFileInfoWithSHA256(pflrun.PFLRun):
+    """Derived class for scanning and storage of file information including SHA256 hash."""
 
     def __init__(self, params):
         super().__init__(params)
-        self.Columns = ["path", "filename", "size", "ctime", "wtime", "md5"]
+        self.Columns = ["path", "filename", "size", "ctime", "wtime", "hash"]
         self._fileDateTimeFormat = "%Y-%m-%d %H:%M:%S"
 
     def getMatchDataList(self, match):
         """Return list with data from the match."""
         try:
-            md5bytes = self.md5(match)
+            hashbytes = self.getHashBytes(match)
             return [
                 match.parent,
                 match.name,
                 os.path.getsize(match),
                 datetime.fromtimestamp(os.path.getctime(match)),
                 datetime.fromtimestamp(os.path.getmtime(match)),
-                md5bytes,
+                hashbytes,
             ]
         except Exception:
             return [match.parent, match.name, -1, None, None]
 
-    def md5(self, filename, blocksize=65536):
-        """Return the MD5 hash of the file."""
-        hashmd5 = hashlib.md5()  # create a new object for each file!
+    def getHashBytes(self, filename, blocksize=65536):
+        """Return the SHA256 hash of the file."""
+        hashsha = hashlib.sha256()  # create a new object for each file!
         with open(filename, "rb") as f:
             for block in iter(lambda: f.read(blocksize), b""):
-                hashmd5.update(block)
-            # requires Python >v3.7: digest = hashlib.file_digest(f, "md5")
-        return hashmd5.digest()
+                hashsha.update(block)
+            # requires Python >v3.7: digest = hashlib.file_digest(f, "sha256")
+        return hashsha.digest()
 
     def formatListStrings(self, dataList):
         """Return all elements in the list formatted as strings."""
@@ -85,7 +85,7 @@ class PFLRunFileInfoWithMD5(pflrun.PFLRun):
 # (kept outside try-catch block to leave exception messages untouched)
 parser = pflargparse.PFLArgParseWUserPattern(
     description="List files matching a pattern in a directory and its sub-directories,\n"
-    + "and print results including file information with MD5 to stdout,\n"
+    + "and print results including file information with SHA256 to stdout,\n"
     + "or save as a CSV or database file."
 )
 args = parser.parse_args()
@@ -108,7 +108,7 @@ try:
         )
     )
 
-    run = PFLRunFileInfoWithMD5(params)
+    run = PFLRunFileInfoWithSHA256(params)
 
     run.Run(False)
 except (KeyboardInterrupt):
